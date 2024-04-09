30e80f27297573ec0ac92ca31a3857f15240cd7b
IVAN AMADO CARDOSO <peacevan@hotmail.com>
2024-04-09 05:13:01 -0300

    feat: :sparkles: add route file

---
+++ router/routes.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	docs "github.com/arthur404dev/gopportunities/docs"
+	"github.com/arthur404dev/gopportunities/handler"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
+	{
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
+
+	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
